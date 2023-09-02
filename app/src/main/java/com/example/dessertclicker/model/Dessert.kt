672fc9a3d39b16d11096296e5368b10836a48830
Cristina Jung <cristijung@gmail.com>
2023-09-02 15:16:49 -0300

    inicio de trabalho

--- app/src/main/java/com/example/dessertclicker/model/Dessert.kt
+++ app/src/main/java/com/example/dessertclicker/model/Dessert.kt
@@ -2,6 +2,6 @@
 package com.example.dessertclicker.model
 
 /**
- * [Dessert] é a classe de dados para representar o imageId, o preço e o startProductionAmount da sobremesa
+ * [Dessert] Esta é a classe de dados para representar o imageId, o preço e o startProductionAmount da sobremesa
  */
 data class Dessert(val imageId: Int, val price: Int, val startProductionAmount: Int)
